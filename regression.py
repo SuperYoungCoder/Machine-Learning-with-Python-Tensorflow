1f0095ff6d8295d70497c542445a1ba5f3d1f44e
Joseph YJ Chung <young.jo.chung@gmail.com>
2022-06-08 18:19:11 -0400

    Update regression.py

--- regression.py
+++ regression.py
@@ -14,3 +14,13 @@ from tensorflow.keras import layers
 
 print(tf.__version__)
 
+# Get the data
+url = 'http://archive.ics.uci.edu/ml/machine-learning-databases/auto-mpg/auto-mpg.data'
+column_names = ['MPG', 'Cylinders', 'Displacement', 'Horsepower', 'Weight', 'Acceleration;, 'Model Year', 'Origin']
+
+raw_dataset = pd.read_csv(url, names=column_names,
+                          na_values='?', comment='\t',
+                          sep=' ', skipinitialspace=True)
+
+dataset = raw_dataset.copy()
+dataset.tail()
